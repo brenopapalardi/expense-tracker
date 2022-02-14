99fc5d60018afe1881f79be55b398cecc7ad8866
Breno Papalardi <brenop.developer@gmail.com>
2022-02-14 08:55:20 -0300

    finish project

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2022, 1,  12), category: 'food', title:'McDonalds', value: 32.12 },
+    { date: new Date(2022, 1,  12), category: 'food', title:'Burguer King', value: 28 },
+    { date: new Date(2022, 1,  13), category: 'rent', title:'Aluguel Apt', value: 2300 },
+    { date: new Date(2022, 2,  18), category: 'salary', title:'Salário IGC', value: 4500},
+]
